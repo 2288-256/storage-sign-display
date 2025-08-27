5bc8a1c3d90d528034a9b2ea104b4fd83dce0f32
2288-256 <74705442+2288-256@users.noreply.github.com>
2025-08-28 02:21:23 +0900

    chore: remove unnecessary blank line in ItemSizeDisplayMixin.java

--- src/client/java/com/github/a2288/storage_sign_display/client/mixin/ItemSizeDisplayMixin.java
+++ src/client/java/com/github/a2288/storage_sign_display/client/mixin/ItemSizeDisplayMixin.java
@@ -17,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
-
 @Mixin(ItemRenderer.class)
 public abstract class ItemSizeDisplayMixin {
 
